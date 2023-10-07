1a9dcc39702a6c37059b416083f8f7d8a42dfc7d
taeoldest <alextaeyounglee@gmail.com>
2023-10-07 17:54:27 -0500

    Updates

--- src/components/Footer.jsx
+++
@@ -1,1 +0,0 @@
-// to be used for filters
--- src/components/Text.jsx
+++
@@ -1,8 +0,0 @@
-// format for text
-export function Text({ children, justification }) {
-    return (
-        <p className={`text-black text-2xl ${justification}`}>
-            {children}
-        </p>
-    )
-}
